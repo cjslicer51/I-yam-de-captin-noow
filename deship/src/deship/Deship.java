4e540f728d361289a04f0978e947b5137ae88d0c
Cjparke11@gmail.com <>
2020-02-06 13:59:54 -0500

    Cory Parke - new static void, added steps on maintaining ship

--- deship/src/deship/Deship.java
+++ deship/src/deship/Deship.java
@@ -15,7 +15,11 @@ public class Deship {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        maintain();
         // TODO code application logic here
     }
-    
+    public static void maintain(){
+        System.out.println("Clean the poopdeck");
+        System.out.println("Maintain the sails");
+    }
 }
